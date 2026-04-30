a8064f86391ead6788c728670727c5e5f09e8156
Burke Bartlett <BurkeBart257867lett@users.noreply.github.com>
2026-04-30 08:21:16 +0800

    feat: add package __init__ with version detection and public API exports

---
+++ chandra/__init__.py
@@ -0,0 +1,21 @@
+"""Chandra - A fast, lightweight text embedding library.
+
+Fork of datalab-to/chandra with additional optimizations
+and extended model support.
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("chandra")
+except PackageNotFoundError:
+    __version__ = "0.0.0-dev"
+
+from chandra.encoder import Encoder
+from chandra.model import EmbeddingModel
+
+__all__ = [
+    "Encoder",
+    "EmbeddingModel",
+    "__version__",
+]
